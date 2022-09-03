5e5014a9bd507a07fe6bad4876e89a7ea04d06c0
Marco Kreeft <marco.kreeft@gmail.com>
2022-09-03 18:09:38 +0200

    v1.1.1 because HA caches too much

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { getEntityIds, hasConfigOrEntitiesChanged, hideIf } from './util';
 import { style } from './styles';
 
 console.info(
-    '%c ROOM-CARD %c 1.1.0',
+    '%c ROOM-CARD %c 1.1.1',
     'color: cyan; background: black; font-weight: bold;',
     'color: darkblue; background: white; font-weight: bold;'
 );
