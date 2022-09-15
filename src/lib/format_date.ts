bc0c4fc2223505bee88abf228e9b2b1ce98a87f3
Marco Kreeft <m.kreeft@dictu.nl>
2022-09-15 20:59:56 +0200

    Fix for last_changed and last_updated

--- src/lib/format_date.ts
+++ src/lib/format_date.ts
@@ -1,15 +1,8 @@
 // Source: https://github.com/home-assistant/frontend/blob/dev/src/common/datetime/format_date.ts
-
 import { FrontendLocaleData } from 'custom-card-helpers';
-import memoizeOne from 'memoize-one';
 
-export const formatDate = (dateObj: Date, locale: FrontendLocaleData) => formatDateMem(locale).format(dateObj);
-
-const formatDateMem = memoizeOne(
-    (locale) =>
-        new Intl.DateTimeFormat(locale.language, {
-            year: 'numeric',
-            month: 'long',
-            day: 'numeric',
-        })
-);
+export const formatDate = (dateObj: Date, locale: FrontendLocaleData) => new Intl.DateTimeFormat(locale.language, {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+}).format(dateObj);
