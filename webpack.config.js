7470720ee5cbcbae6b571bc40f58abac9ea5cc79
Marco Kreeft <marco.kreeft@gmail.com>
2022-09-21 19:25:56 +0200

    webpack back to normal

--- webpack.config.js
+++ webpack.config.js
@@ -9,9 +9,6 @@ module.exports = {
         filename: 'room-card.js',
         path: path.resolve(__dirname),
     },
-    optimization: {
-        minimize: false,
-    },
     module: {
         rules: [
             {
