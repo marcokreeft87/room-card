b7570d0eb01a3fe30fc59270d63bcce8935a9ea4
Marco Kreeft <marco.kreeft@gmail.com>
2022-11-23 11:21:41 +0100

    Fix hide if entities not triggering update

--- tests/util/getConditionEntities.test.ts
+++ tests/util/getConditionEntities.test.ts
@@ -6,14 +6,25 @@ import { StubHomeAssistantEntity } from '../testdata';
 describe('Testing util file function getConditionEntities', () => {
     test('Passing undefined should return empty array', () => {   
         expect(getConditionEntities(undefined)).toEqual([]);
-    })//,
-    // test('Passing undefined should return', () => {   
-    //     const entities: RoomCardEntity[] = [{
-    //         stateObj: StubHomeAssistantEntity,
-    //         icon: {
-    //             conditions: [undefined as EntityCondition]
-    //         }
-    //     }]
-    //     expect(getConditionEntities(entities)).toEqual([]);
-    // })
+    }),
+    test('Passing undefined should return', () => {   
+        const entities: RoomCardEntity[] = [{
+            stateObj: StubHomeAssistantEntity,
+            icon: {
+                conditions: [{
+                    entity: 'test.entity'
+                }]
+            },
+            hide_if: {
+                conditions: [{
+                    entity: 'test.entity2'
+                }]
+            }
+        }]
+        expect(getConditionEntities(entities)).toEqual([{
+            entity: 'test.entity'
+        }, {
+            entity: 'test.entity2'
+        }]);
+    })
 })  
