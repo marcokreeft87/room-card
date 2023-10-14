71967d240effa9f7ac27112141e57d870cd84a7e
Marco Kreeft <marco.kreeft@gmail.com>
2023-10-14 13:22:12 +0200

    -

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -452,6 +452,7 @@ describe('Testing index file class RoomCard', () => {
             show_states: ['test'],
             type: 'custom'
         }
+        // eslint-disable-next-line @typescript-eslint/no-empty-function, @typescript-eslint/no-unused-vars
         roomcard._helpers = { createCardElement(_config: LovelaceCardConfig): LovelaceCard {
             const element = {
                 style: {}
